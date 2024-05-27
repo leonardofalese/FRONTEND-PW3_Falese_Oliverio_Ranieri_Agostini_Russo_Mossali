064d23940254a12bb12b7bc0da3668b397a0c1e1
Your Name <you@example.com>
2024-05-27 10:27:30 +0200

    Modifiche fetch in amministratore Modifiche per la visualizzazione delle informazioni dell'amministratore

--- app/amministratore/page.js
+++ app/amministratore/page.js
@@ -11,51 +11,73 @@ import { useRouter } from 'next/navigation';
 export default function Amministratore() {
     const router = useRouter();
 
-    const amministratore = {
-        id: 1,
-        nome: "Mario",
-        cognome: "Rossi",
-        email: "mario@example.com",
-    };
+    try{
+        const response = fetch('http://localhost:8080/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({id, nome, cognome, email}),
+        });
+
+        if(response.ok) {
+            const data = response.json();
+            console.log('Login succesful', data);
+            if(data.ruolo == "ADMIN") {
+                //fai qualcosa
+                const amministratore = {
+                    id: {id},
+                    nome: {nome},
+                    cognome: {cognome},
+                    email: {email}
+                };
+                return (
+                    <body>
+                        <Header />
+                        <main className={styles.main}>
+                            <section className={styles.section}>
+                                <div className={styles.container}>
+                                    <h1 className={styles.h1}>Benvenuto Amministratore</h1>
+                                    <hr className={styles.hr}></hr>
+                                    <div className={styles.info}>
+                                        <p className={styles.p}><strong>ID:</strong> {amministratore.id}</p>
+                                        <p className={styles.p}><strong>Nome:</strong> {amministratore.nome}</p>
+                                        <p className={styles.p}><strong>Cognome:</strong> {amministratore.cognome}</p>
+                                        <p className={styles.p}><strong>Email:</strong> {amministratore.email}</p>
+                                    </div>
+                                </div>
+                                <div className={styles.container2}>
+                                    <p className={styles.h1}>Cosa vuoi visualizzare?</p>
+                                    <div className={styles.buttons}>
+                                        <div className={styles.button}>
+                                            <button className={styles.container3} onClick={() => navigateTo('/utenti')}>Utenti Registrati
+                                                <Image className={styles.image} src={Utente} alt='Utente Registrati'></Image>
+                                            </button>
+                                        </div>
+                                        <div className={styles.button}>
+                                            <button className={styles.container3} onClick={() => navigateTo('/corsiAmministratore')}>Corsi disponibili
+                                                <Image className={styles.image} src={Corsi} alt='Corsi Disponibili'></Image>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+            
+                            </section>
+                        </main>
+                        <Footer />
+                    </body>
+                );
+            } else {
+                //fai altro
+            }
+        }else{
+            console.log('Login failed', response.statusText);
+        }
+    }catch(error){
+        console.log('Error:', error);
+    }
 
     const navigateTo = (path) => {
         router.push(path);
     };
-
-    return (
-        <body>
-            <Header />
-            <main className={styles.main}>
-                <section className={styles.section}>
-                    <div className={styles.container}>
-                        <h1 className={styles.h1}>Benvenuto Amministratore</h1>
-                        <hr className={styles.hr}></hr>
-                        <div className={styles.info}>
-                            <p className={styles.p}><strong>ID:</strong> {amministratore.id}</p>
-                            <p className={styles.p}><strong>Nome:</strong> {amministratore.nome}</p>
-                            <p className={styles.p}><strong>Cognome:</strong> {amministratore.cognome}</p>
-                            <p className={styles.p}><strong>Email:</strong> {amministratore.email}</p>
-                        </div>
-                    </div>
-                    <div className={styles.container2}>
-                        <p className={styles.h1}>Cosa vuoi visualizzare?</p>
-                        <div className={styles.buttons}>
-                            <div className={styles.button}>
-                                <button className={styles.container3} onClick={() => navigateTo('/utenti')}>Utenti Registrati
-                                    <Image className={styles.image} src={Utente} alt='Utente Registrati'></Image>
-                                </button>
-                            </div>
-                            <div className={styles.button}>
-                                <button className={styles.container3} onClick={() => navigateTo('/corsiAmministratore')}>Corsi disponibili
-                                    <Image className={styles.image} src={Corsi} alt='Corsi Disponibili'></Image>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                </section>
-            </main>
-            <Footer />
-        </body>
-    );
 }
