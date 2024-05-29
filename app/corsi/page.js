a62383b059381b53721d455a5e76a9a1d75bf771
Mattia <mossalimattia@gmail.com>
2024-05-29 10:03:39 +0200

    Sviluppo della chiamata che permette la candidatura ai corsi con gestione di errori ed eccezioni

--- app/corsi/page.js
+++ app/corsi/page.js
@@ -1,8 +1,8 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 
-import Header from '@/app/corsi/components/header'
+import Header from '@/app/amministratore/components/header';
 import Footer from '@/components/footer'
 
 import styles from './page.module.css'
@@ -43,9 +43,61 @@ export default function Corsi() {
 
     }, []);
 
+    const handleLogin = async (courseName) => {
+        try {
+            const response = await fetch('http://localhost:8080/users', {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (response.ok) {
+                const user = await response.json();
+                if (user.role === 'STUDENT') {
+                    console.log('Utente riconosciuto, procedi con la candidatura!');
+                    await handleApplication(courseName);
+                } else if (user.role === 'ADMIN') {
+                    alert('Utente riconosciuto come amministratore, non può candidarsi ad un corso');
+                    console.log('Utente riconosciuto come amministratore, non può candidarsi ad un corso');
+                }
+            } else {
+                const errorMessage = await response.text();
+                console.log('Errore nel recupero delle informazioni dell\'utente loggato');
+                alert('Assicurati di effettuare il login per candidarti ad un corso!');
+            }
+        } catch (error) {
+            console.log('Errore:', error);
+        }
+    }
+
+    const handleApplication = async (courseName) => {
+        try {
+            const response = await fetch('http://localhost:8080/applications', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({courseName}),
+            });
+
+            if (response.ok) {
+                alert('Candidatura inviata con successo!');
+            } else {
+                const errorMessage = await response.text();
+                alert('Errore, la candidatura è già stata effettuata per il corso scelto:', response.status, errorMessage);
+                console.log('Errore, la candidatura è già stata effettuata per il corso scelto:', response.status, errorMessage);
+            }
+        } catch (error) {
+            console.log('Errore:', error);
+        }
+    }
+
     return (
         <div>
-            <Header />
+            <Header/>
             <h1 className={styles.h1}>Scopri i nostri corsi</h1>
             <div className={styles.body}>
                 {Object.entries(corsi).map(([categoria, corsiCategoria]) => (
@@ -59,7 +111,7 @@ export default function Corsi() {
                                             <div className={styles.content}>
                                                 <h3>{corso.name}</h3>
                                                 <p>{corso.category}</p>
-                                                <a href="#">Candidati!</a>
+                                                <a href="#" onClick={() => handleLogin(corso.name)}>Candidati!</a>
                                             </div>
                                         </div>
                                     </div>
@@ -69,7 +121,7 @@ export default function Corsi() {
                     </div>
                 ))}
             </div>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
