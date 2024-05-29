908e384cc4d30da3f718552b454b4415c49cd049
Your Name <you@example.com>
2024-05-29 09:13:21 +0200

    Modifica pagina profilo studente modifica informazioni studente

--- app/profile/page.js
+++ app/profile/page.js
@@ -11,8 +11,6 @@ export default function Profile() {
     firstName: "Mario",
     lastName: "Rossi",
     email: "mario.rossi@gmail.com",
-    dob: "24-03-2004",
-    phone: "123-456-7890",
     course: "Web Developer",
   };
 
@@ -34,14 +32,6 @@ export default function Profile() {
             <div className={styles.profileField}>
               <label htmlFor="email">Email:</label>
               <span id="email">{profileInfo.email}</span>
-            </div>
-            <div className={styles.profileField}>
-              <label htmlFor="dob">DATA DI NASCITA:</label>
-              <span id="dob">{profileInfo.dob}</span>
-            </div>
-            <div className={styles.profileField}>
-              <label htmlFor="phone">NUMERO DI TELEFONO:</label>
-              <span id="phone">{profileInfo.phone}</span>
             </div>
             <div className={styles.profileField}>
               <label htmlFor="course">CORSO:</label>
